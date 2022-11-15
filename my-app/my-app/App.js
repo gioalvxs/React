71f5702e4e192b9424d11d7eed4af46cf65dca21
Giovana <104846007+gioalvxs@users.noreply.github.com>
2022-11-14 23:26:19 -0300

    Add files via upload

---
+++ my-app/my-app/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <img src={require('./assets/anya.jpg')} 
+      style={{ width: 420, height: 420 }}/>
+      <Text>Giovana Alves.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
